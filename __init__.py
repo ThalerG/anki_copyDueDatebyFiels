e2beaf00a57d71e73c4208420d48556106729e31
ThalerG <thalergabriel@gmail.com>
2024-08-27 11:28:04 -0300

    Add in-window action

--- __init__.py
+++ __init__.py
@@ -1,25 +1,25 @@
 from anki.collection import Collection
-# from anki.hooks import addHook
-# # import the main window object (mw) from aqt
-# from aqt import mw
-# # import the "show info" tool from utils.py
-# from aqt.utils import showInfo, qconnect
-# # import all of the Qt GUI library
-# from aqt.qt import *
+from anki.hooks import addHook
+# import the main window object (mw) from aqt
+from aqt import QAction, mw
+# import the "show info" tool from utils.py
+from aqt.utils import showInfo, qconnect
+# import all of the Qt GUI library
+from aqt.qt import *
 
 # We're going to add a menu item below. First we want to create a function to
 # be called when the menu item is activated.
 
-# def testFunction() -> None:
-#     col = mw.col
-#     transfer_scheduling_data(col, 'Hanzi', 'Domino Text Input-43bf8', 'Recall', 'Domino Recognition and Stroke Order-6c462', 'Recall')
+def testFunction() -> None:
+    col = mw.col
+    transfer_scheduling_data(col, 'Hanzi', 'Domino Text Input-43bf8', 'Recall', 'Domino Recognition and Stroke Order-6c462', 'Recall')
 
-# # create a new menu item, "test"
-# action = QAction("Copy cards", mw)
-# # set it to call testFunction when it's clicked 
-# qconnect(action.triggered, testFunction)
-# # and add it to the tools menu
-# mw.form.menuTools.addAction(action)
+# create a new menu item, "test"
+action = QAction("Copy cards", mw)
+# set it to call testFunction when it's clicked 
+qconnect(action.triggered, testFunction)
+# and add it to the tools menu
+mw.form.menuTools.addAction(action)
 
 import time
 
